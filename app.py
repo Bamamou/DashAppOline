f8e0b0c1044f67e871b06202dbd1dda77f0efb25
CecedeKP <bamamounicolas@gmail.com>
2024-06-24 17:43:21 +0800

    V1.0.1 the second m=commit with the requirements file added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 app = Dash(__name__)
+server = app.server
 #df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 df = pd.read_csv('C:\\Users\\admin\\Documents\\GitHub\\Pyhton_Jupiter\\Data\\Battery\\Balistron1.txt',  header=None, na_values=[''], skipinitialspace=True)
 # Iterate through each column
