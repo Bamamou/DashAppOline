5382cf6b76151c946ee28afb8bc379f24253f717
CecedeKP <bamamounicolas@gmail.com>
2024-06-24 17:57:08 +0800

    V1.0.3

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 app = Dash(__name__)
 server = app.server
 #df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-df = pd.read_csv('C:\\Users\\admin\\Documents\\GitHub\\Pyhton_Jupiter\\Data\\Battery\\Balistron1.txt',  header=None, na_values=[''], skipinitialspace=True)
+df = pd.read_csv('C:\Users\admin\Documents\GitHub\DASH_ESP32\DashAppOline\Balistron1.txt',  header=None, na_values=[''], skipinitialspace=True)
 # Iterate through each column
 for col in df.columns:
     Data =df[col].str.split(';', expand=True)
