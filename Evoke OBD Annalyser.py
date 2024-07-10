434764298201892e854c2b006e322821fbcf84cf
ShahidulSaad <kmsaad01711@gmail.com>
2024-07-10 15:29:38 +0800

    V1.3

--- Evoke OBD Annalyser.py
+++ Evoke OBD Annalyser.py
@@ -59,19 +59,20 @@ def upload_file(contents, filename):
             df[col] =(df[col]+200)/100
             df.rename(columns={col:'String'+str(col-29)}, inplace = True)
         # Let's also rename the temperature columns
-        for col in df.iloc[:, 62:67]:
+        for col in df.iloc[:, 62:68]:
             df.rename(columns={col:'Temperature'+str(col-62)}, inplace = True)
         # divide the below values by 10
         df[29] =df[29].div(10)   # BMS pack voltgae 
         df[81] =df[81].div(10)   #MCU pack voltage
         df[10] =df[10].div(10)   #ECU supply voltage
-        df.rename(columns = {1:'packetCode', 2:'second', 3:'minute', 4: 'hour', 5:'dayW', 6:'dayM',  7:'month',  8:'year', 9:'boardTemperature', 10:'boardSupplyVoltage',
-                       11:'odometerKm', 12:'tripKm', 13:'speedKmh',  14:'maximumSpeed', 15:'rpm', 16:'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2',
-                       19:'numActiveErrors', 20:'sumActiveErrors', 21:'roll', 22:'pitch', 23:'frontTirePressure', 24:'frontTireTemperature', 25:'rearTirePressure', 26:'rearTireTemperature', 27:'inputsI2C',
-                       28:'outputsI2C', 29:'Pack Voltage', 9:'boardTemperature', 10:'boardSupplyVoltage', 11:'odometerKm', 12:'tripKm', 13:'speedKmh',
-                       14:'maximumSpeed',  16: 'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2', 69:'SOC', 62:'Pack_DSG_Current',
-                       85:'Invt_Temp', 83:'RPM', 81:'MCU_Voltage', 84:'Motor_Temp'}, inplace = True)
-
+        df.rename(columns = {1:'packetCode', 2:'second', 3:'minute', 4: 'hour', 5:'dayW', 6:'dayM',  7:'month',  8:'year', 9:'boardTemperature', 
+                        10:'boardSupplyVoltage', 11:'odometerKm', 12:'tripKm', 13:'speedKmh',  14:'maximumSpeed', 15:'rpm', 16:'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2', 19:'numActiveErrors', 
+                        20:'sumActiveErrors', 21:'roll', 22:'pitch', 23:'frontTirePressure', 24:'frontTireTemperature', 25:'rearTirePressure', 26:'rearTireTemperature', 27:'inputsI2C', 28:'outputsI2C', 29:'Pack Voltage',  
+                        62:'Pack_DSG_Current', 69:'SOC', 
+                        70:'BatteryPhysicalCapacity', 71:'BatteryRemainingCapacity', 72:'BMSTurnOnTime', 73:'CellHighestVoltageNumber', 74:'CellHighestVoltageValue', 75:'CellLowestVoltageNumber', 76:'CellLowestVoltageValue', 77:'NumberOfBatteries', 78:'ChargingMosfetStatus', 79:'DischargingMosfetStatus', 
+                        80:'BalanceStateSign', 81:'MCU_Voltage', 82:'MCUBus_Current', 83:'RPM', 84:'Motor_Temp', 85:'Invt_Temp', 89:'MCU_state', 
+                        90:'Throttle', 91:'LowByteError', 92:'HighByteError', 93:'StatusByte', 94:'MCUOutCurrent', 95:'MCUMaxRegen', 96:'maximumThrottle', 97:'minimumThrottle', 98:'chargerVoltage', 99:'chargerCurrent', 
+                        100:'chargerStatusByte', 101:'maxChargerVoltage', 102:'Fan_Speed', 103:'millis', 104:'resetReason'}, inplace = True)
 
         # df.rename(columns = {29:'Pack Voltage', 9:'boardTemperature', 10:'boardSupplyVoltage', 11:'odometerKm', 12:'tripKm', 13:'speedKmh',
         #                14:'maximumSpeed',  16: 'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2', 69:'SOC', 62:'Pack_DSG_Current',
@@ -107,20 +108,21 @@ def plot_selected_columns(selected_columns, contents):
             df[col] =(df[col]+200)/100
             df.rename(columns={col:'String'+str(col-29)}, inplace = True)
         # Let's also rename the temperature columns
-        for col in df.iloc[:, 62:67]:
+        for col in df.iloc[:, 62:68]:
             df.rename(columns={col:'Temperature'+str(col-62)}, inplace = True)
         #Divide the pack voltage per 10   
         df[29] =df[29].div(10)   # BMS pack voltgae 
         df[81] =df[81].div(10)   #MCU pack voltage
         df[10] =df[10].div(10)   #ECU supply voltage
         #Rename some columns
-        df.rename(columns = {1:'packetCode', 2:'second', 3:'minute', 4: 'hour', 5:'dayW', 6:'dayM',  7:'month',  8:'year', 9:'boardTemperature', 10:'boardSupplyVoltage',
-                       11:'odometerKm', 12:'tripKm', 13:'speedKmh',  14:'maximumSpeed', 15:'rpm', 16:'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2',
-                       19:'numActiveErrors', 20:'sumActiveErrors', 21:'roll', 22:'pitch', 23:'frontTirePressure', 24:'frontTireTemperature', 25:'rearTirePressure', 26:'rearTireTemperature', 27:'inputsI2C',
-                       28:'outputsI2C', 29:'Pack Voltage', 9:'boardTemperature', 10:'boardSupplyVoltage', 11:'odometerKm', 12:'tripKm', 13:'speedKmh',
-                       14:'maximumSpeed',  16: 'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2', 69:'SOC', 62:'Pack_DSG_Current',
-                       85:'Invt_Temp', 83:'RPM', 81:'MCU_Voltage', 84:'Motor_Temp'}, inplace = True)
-
+        df.rename(columns = {1:'packetCode', 2:'second', 3:'minute', 4: 'hour', 5:'dayW', 6:'dayM',  7:'month',  8:'year', 9:'boardTemperature', 
+                        10:'boardSupplyVoltage', 11:'odometerKm', 12:'tripKm', 13:'speedKmh',  14:'maximumSpeed', 15:'rpm', 16:'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2', 19:'numActiveErrors', 
+                        20:'sumActiveErrors', 21:'roll', 22:'pitch', 23:'frontTirePressure', 24:'frontTireTemperature', 25:'rearTirePressure', 26:'rearTireTemperature', 27:'inputsI2C', 28:'outputsI2C', 29:'Pack Voltage',  
+                        62:'Pack_DSG_Current', 69:'SOC', 
+                        70:'BatteryPhysicalCapacity', 71:'BatteryRemainingCapacity', 72:'BMSTurnOnTime', 73:'CellHighestVoltageNumber', 74:'CellHighestVoltageValue', 75:'CellLowestVoltageNumber', 76:'CellLowestVoltageValue', 77:'NumberOfBatteries', 78:'ChargingMosfetStatus', 79:'DischargingMosfetStatus', 
+                        80:'BalanceStateSign', 81:'MCU_Voltage', 82:'MCUBus_Current', 83:'RPM', 84:'Motor_Temp', 85:'Invt_Temp', 89:'MCU_state', 
+                        90:'Throttle', 91:'LowByteError', 92:'HighByteError', 93:'StatusByte', 94:'MCUOutCurrent', 95:'MCUMaxRegen', 96:'maximumThrottle', 97:'minimumThrottle', 98:'chargerVoltage', 99:'chargerCurrent', 
+                        100:'chargerStatusByte', 101:'maxChargerVoltage', 102:'Fan_Speed', 103:'millis', 104:'resetReason'}, inplace = True)
 
         # df.rename(columns = {29:'Pack Voltage', 9:'boardTemperature', 10:'boardSupplyVoltage', 11:'odometerKm', 12:'tripKm', 13:'speedKmh',
         #                14:'maximumSpeed',  16: 'efficiency', 17:'vehicleStatuByte1', 18:'vehicleStatuByte2', 69:'SOC', 62:'Pack_DSG_Current',
